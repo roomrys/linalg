7a65e46a74df988adac493d08d9f8ab511c772ce
roomrys <38435167+roomrys@users.noreply.github.com>
2025-09-23 15:10:27 -0700

    Add CONFIG

---
+++ scripts/constants.js
@@ -0,0 +1,21 @@
+export const CONFIG = {
+  SVG: {
+    WIDTH: 400,
+    HEIGHT: 400,
+    CENTER_X: 200,
+    CENTER_Y: 200,
+  },
+  GRID: {
+    BASE_SPACING: 20,
+    SCALE: 20,
+  },
+  VECTOR: {
+    ARROWHEAD_LENGTH: 3,
+    MIN_VECTOR_LENGTH: 5,
+  },
+  MATRIX: {
+    MIN_VALUE: -3,
+    MAX_VALUE: 3,
+    STEP: 0.1,
+  },
+};
